f03b8c0a10502d381a9b9b79e8dc9e60885b5712
Tom Sydney Kerckhove <syd@cs-syd.eu>
2026-06-01 12:49:04 +0200

    Upgrade to nixpkgs version nixos-26.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Haskell Dependency Graph Nix";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-26.05";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
